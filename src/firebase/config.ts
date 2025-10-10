1bdcf2ee620af39ef117d076e626327bff9e27ac
Ivan Alexis Crisanto Montero <iacrismo16@gmail.com>
2025-10-10 07:09:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4560916840-4310c",
+  "appId": "1:277272711271:web:cbaf677c28d6e2541f84c2",
+  "apiKey": "AIzaSyB_J8I-7ySJsU4Q6W7ikQVhBBLxtROE8bE",
+  "authDomain": "studio-4560916840-4310c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "277272711271"
+};
