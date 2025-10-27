addaacbd754f09449db162f68f1d925fac431bc2
crack2116 <crack2116@users.noreply.github.com>
2025-10-27 13:06:58 -0500

    Agregar configuración de Firebase Storage y reglas de seguridad

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:277272711271:web:cbaf677c28d6e2541f84c2",
   "apiKey": "AIzaSyB_J8I-7ySJsU4Q6W7ikQVhBBLxtROE8bE",
   "authDomain": "studio-4560916840-4310c.firebaseapp.com",
+  "storageBucket": "studio-4560916840-4310c.appspot.com",
   "measurementId": "",
   "messagingSenderId": "277272711271"
 };
